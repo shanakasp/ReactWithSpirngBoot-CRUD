d9ab03845900c11799392056a33bd22bf8cb42b8
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-24 11:15:44 +0530

    Add files via upload

---
+++ React - Frontend-Updated/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import ListEmployeeComponent from "./page/AdminDashboard/ListEmployeeComponent";
+import ViewEmployeeComponent from "./page/AdminDashboard/ViewEmployeeComponent";
+import CreateEmployeeComponent from "./page/AdminDashboard/CreateEmployeeComponent";
+
+
+
+function App() {
+ 
+  return (
+    <div>
+ 
+      <Routes>
+      
+        <Route path="/admindashb" element={<ListEmployeeComponent />} />
+        <Route path="/admindashb/add-employee" element={<CreateEmployeeComponent />} />
+        <Route path="/admindashb/view-employee/:id" element={<ViewEmployeeComponent />} />
+    
+       
+     
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
